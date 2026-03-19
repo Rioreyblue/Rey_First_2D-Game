fbaa0a93dcf7623cff8b6588fc7a1d5535274d83
Rioreyblue <franciscorey702@gmail.com>
2026-03-19 23:31:55 +0800

    Initial commit: Only tracking PlayerLander.cs

---
+++ Assets/Scripts/PlayerLander.cs
@@ -0,0 +1,33 @@
+using System.Runtime.Versioning;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerLander : MonoBehaviour
+{
+
+    private Rigidbody2D _playerLander;
+   private void Awake()
+    {
+        _playerLander = GetComponent<Rigidbody2D>();
+        Application.targetFrameRate = 60;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            float force = 800f;
+            _playerLander.AddForce(force * transform.up * Time.deltaTime);
+        }
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            float turnSpeed = +100f;
+            _playerLander.AddTorque(turnSpeed * Time.deltaTime);
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            float turnSpeed = -100f;
+            _playerLander.AddTorque(turnSpeed * Time.deltaTime);
+        }
+    } 
+}
